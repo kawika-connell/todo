42a90da43620916949fe1024cf3ad903be2196f9
Kawika Connell <kawika.connell@batalp.com>
2020-01-30 16:33:55 -0800

    Add 'init' command

--- todo.php
+++ todo.php
@@ -4,13 +4,38 @@ namespace KawikaConnell\Todo;
 
 require __DIR__.'/vendor/autoload.php';
 
+if(!defined("STDIN")) {
+    define("STDIN", fopen('php://stdin','rb'));
+}
+
+const TODO_VERSION = '0.1.0';
+
 const COMMAND_INDEX = '';
 const COMMAND_INDEX_MESSAGE = <<<DOC
 Todo CLI - Version 0.1.0
 
 Usage:
   command [options] [arguments]
+
+  Options:                                                                                                                                                                                                        
+    -h, --help     Display this help message
+    -q, --quiet    Do not output any message
+    -V, --version  Display this application version
+    -g, --global   Run operation globally
+
+  Available commands:
+    init  Creates a todo list in the current directory
 DOC;
+
+/**
+ * Command used to initialize todo list.
+ */
+const    COMMAND_INIT = 'init';
+function command_init(Input $input) {
+    $currentDirectory = getcwd();
+    file_put_contents('todo.txt', '');
+    echo "Created todo.txt in {$currentDirectory}";
+}
 
 $input = new Input(tail($argv));
 $command = $input->getCommand() ?? COMMAND_INDEX;
@@ -20,6 +45,10 @@ switch ($command) {
         echo COMMAND_INDEX_MESSAGE;
         break;
 
+    case COMMAND_INIT:
+        command_init($input);
+        break;
+
     case '':
         break;
 
@@ -27,3 +56,5 @@ switch ($command) {
         echo "Command unknown";
         break;
 }
+
+echo "\n";
